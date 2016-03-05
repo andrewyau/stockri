8b5749bf9f5c43721dddf7c4ebadb65153154534
andrew yau <yauandrew@gmail.com>
2016-03-05 16:28:40 +0000

    Add nuget packages

---
+++ StockTrader RI/StockTraderRI/StockTraderRI/EnterpriseLibraryLoggerAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+namespace StockTraderRI
+{
+    public class EnterpriseLibraryLoggerAdapter : ILoggerFacade
+    {
+        public EnterpriseLibraryLoggerAdapter()
+        {
+            Logger.SetLogWriter(new LogWriter(new LoggingConfiguration()));
+        }
+
+        #region ILoggerFacade Members
+
+        public void Log(string message, Category category, Priority priority)
+        {
+            Logger.Write(message, category.ToString(), (int)priority);
+        }
+
+        #endregion
+    }
+}
